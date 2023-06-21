66963a399852227fc62507be832a19a0f957ecf4
Eric Zhang <ekzhang1@gmail.com>
2023-06-21 11:44:13 -0500

    Working CodeMirror and Markdown plumbing

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,122 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import {
+  // lineNumbers,
+  // highlightActiveLineGutter,
+  highlightSpecialChars,
+  drawSelection,
+  dropCursor,
+  rectangularSelection,
+  crosshairCursor,
+  // highlightActiveLine,
+  keymap,
+} from "@codemirror/view";
+import { EditorView } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import {
+  // foldGutter,
+  indentOnInput,
+  syntaxHighlighting,
+  defaultHighlightStyle,
+  bracketMatching,
+  foldKeymap,
+} from "@codemirror/language";
+import { history, defaultKeymap, historyKeymap } from "@codemirror/commands";
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
+import {
+  closeBrackets,
+  autocompletion,
+  closeBracketsKeymap,
+  completionKeymap,
+} from "@codemirror/autocomplete";
+import { lintKeymap } from "@codemirror/lint";
+import { markdown } from "@codemirror/lang-markdown";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkHtml from "remark-html";
+
+const pipeline = unified().use(remarkParse).use(remarkHtml);
+
+function setText(text: string) {
+  const output = document.getElementById("output")!;
+  const file = pipeline.processSync(text);
+  output.innerHTML = String(file);
+}
+
+const parent = document.getElementById("editor")!;
+
+const theme = EditorView.theme({
+  "&": {
+    fontSize: "13pt",
+    border: "1px solid #e8e8e8",
+    padding: "8px",
+    backgroundColor: "#fafafa",
+    borderRadius: "6px",
+  },
+  "&.cm-focused": {
+    outline: "none",
+    border: "1px solid #d0d0d0",
+  },
+  ".cm-content": {
+    fontFamily: `ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+    // fontFamily: "Menlo, Monaco, Lucida Console, monospace",
+  },
+});
+
+// This is customized from the "codemirror" basicSetup extension.
+const basicSetup = [
+  // lineNumbers(),
+  // highlightActiveLineGutter(),
+  highlightSpecialChars(),
+  history(),
+  // foldGutter(),
+  drawSelection(),
+  dropCursor(),
+  EditorState.allowMultipleSelections.of(true),
+  indentOnInput(),
+  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  crosshairCursor(),
+  // highlightActiveLine(),
+  highlightSelectionMatches(),
+  keymap.of([
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...completionKeymap,
+    ...lintKeymap,
+  ]),
+];
+
+let view = new EditorView({
+  extensions: [
+    theme,
+    basicSetup,
+    EditorView.lineWrapping,
+    markdown(),
+    EditorView.updateListener.of((update) => {
+      if (update.docChanged) {
+        setText(update.state.doc.toString());
+      }
+    }),
+  ],
+  parent,
+});
+
+view.dispatch({
+  changes: {
+    from: 0,
+    to: view.state.doc.length,
+    insert: `# CodeMirror Collaboration
+
+This is a collaborative **Markdown** editor shared by all viewers of this page, who are connected to the backend.
+
+Type here to see the output rendered below.`,
+  },
+});
