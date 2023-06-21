daf22ab38338c37f01711b17653456e6adb3a6e4
Eric Zhang <ekzhang1@gmail.com>
2023-06-21 14:11:45 -0500

    Remove top-level await

--- src/main.ts
+++ src/main.ts
@@ -9,35 +9,39 @@ import { getDocument, peerExtension } from "./collab";
 import { Connection } from "./connection";
 import "./style.css";
 
-const conn = new Connection(
-  (window.location.protocol === "https:" ? "wss://" : "ws://") +
-    window.location.host +
-    "/ws"
-);
-
-let { version, doc } = await getDocument(conn);
-
 function setText(text: string) {
   const output = document.getElementById("output")!;
   const file = unified().use(remarkParse).use(remarkHtml).processSync(text);
   output.innerHTML = String(file);
 }
 
-setText(doc.toString());
+const conn = new Connection(
+  (window.location.protocol === "https:" ? "wss://" : "ws://") +
+    window.location.host +
+    "/ws"
+);
 
-new EditorView({
-  doc,
-  extensions: [
-    theme,
-    basicSetup,
-    EditorView.lineWrapping,
-    markdown(),
-    peerExtension(version, conn),
-    EditorView.updateListener.of((update) => {
-      if (update.docChanged) {
-        setText(update.state.doc.toString());
-      }
-    }),
-  ],
-  parent: document.getElementById("editor")!,
-});
+async function main() {
+  let { version, doc } = await getDocument(conn);
+
+  setText(doc.toString());
+
+  new EditorView({
+    doc,
+    extensions: [
+      theme,
+      basicSetup,
+      EditorView.lineWrapping,
+      markdown(),
+      peerExtension(version, conn),
+      EditorView.updateListener.of((update) => {
+        if (update.docChanged) {
+          setText(update.state.doc.toString());
+        }
+      }),
+    ],
+    parent: document.getElementById("editor")!,
+  });
+}
+
+main();
